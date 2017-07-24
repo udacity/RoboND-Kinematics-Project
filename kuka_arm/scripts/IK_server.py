14422bed6b49b71c25d4cee945986d827ae7c397
robin.han <hanjf_robin@163.com>
2017-07-23 23:22:18 -0700

    modify DH transformation matrix

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -55,15 +55,59 @@ def handle_calculate_IK(req):
 
             
             # Define Modified DH Transformation matrix
-            def DHTransMatrix(alpha, a, d, q):
-            	TMatrix = Matrix([[         cos(q),           -sin(q),     -sin(q),             a],
-								[sin(q)*cos(alpha), cos(q)*cos(alpha), -sin(alpha), -sin(alpha)*d],
-								[sin(q)*sin(alpha), cos(q)*sin(alpha),  cos(alpha),  cos(alpha)*d],
-								[                0,                 0,           0,             1]])
-				return TMatrix
-            
+            T0_1 = Matrix([[            cos(q1),            -sin(q1),     -sin(q1),              a0],
+						   [sin(q1)*cos(alpha0), cos(q1)*cos(alpha0), -sin(alpha0), -sin(alpha0)*d1],
+						   [sin(q1)*sin(alpha0), cos(q1)*sin(alpha0),  cos(alpha0),  cos(alpha0)*d1],
+						   [                  0,                   0,            0,               1]])
+        	T0_1 = T0_1.subs(s)   
+
+
+            T1_2 = Matrix([[            cos(q2),            -sin(q2),     -sin(q2),              a1],
+						   [sin(q2)*cos(alpha1), cos(q2)*cos(alpha1), -sin(alpha1), -sin(alpha0)*d2],
+						   [sin(q2)*sin(alpha1), cos(q2)*sin(alpha1),  cos(alpha1),  cos(alpha0)*d2],
+						   [                  0,                   0,            0,               1]])
+			T1_2 = T1_2.subs(s)
+
+			
+            T2_3 = Matrix([[            cos(q3),            -sin(q3),     -sin(q3),              a2],
+						   [sin(q3)*cos(alpha2), cos(q3)*cos(alpha2), -sin(alpha2), -sin(alpha2)*d3],
+						   [sin(q3)*sin(alpha2), cos(q3)*sin(alpha2),  cos(alpha2),  cos(alpha2)*d3],
+						   [                  0,                   0,            0,               1]])
+			T2_3 = T2_3.subs(s)
+
+			
+            T3_4 = Matrix([[            cos(q4),            -sin(q4),     -sin(q4),              a3],
+						   [sin(q4)*cos(alpha3), cos(q4)*cos(alpha3), -sin(alpha3), -sin(alpha3)*d4],
+						   [sin(q4)*sin(alpha3), cos(q4)*sin(alpha3),  cos(alpha3),  cos(alpha3)*d4],
+						   [                  0,                   0,            0,               1]])
+			T3_4 = T3_4.subs(s)
+
+			
+            T4_5 = Matrix([[            cos(q5),            -sin(q5),     -sin(q5),              a0],
+						   [sin(q5)*cos(alpha4), cos(q5)*cos(alpha4), -sin(alpha4), -sin(alpha4)*d5],
+						   [sin(q5)*sin(alpha4), cos(q5)*sin(alpha4),  cos(alpha4),  cos(alpha4)*d5],
+						   [                  0,                   0,            0,               1]])
+			T4_5 = T4_5.subs(s)
+
+			
+            T5_6 = Matrix([[            cos(q6),            -sin(q6),     -sin(q6),              a5],
+						   [sin(q6)*cos(alpha5), cos(q6)*cos(alpha5), -sin(alpha5), -sin(alpha5)*d6],
+						   [sin(q6)*sin(alpha5), cos(q6)*sin(alpha5),  cos(alpha5),  cos(alpha5)*d6],
+						   [                  0,                   0,            0,               1]])
+			T5_6 = T5_6.subs(s)
+
+			
+            T6_7 = Matrix([[            cos(q7),            -sin(q7),     -sin(q7),              a6],
+						   [sin(q7)*cos(alpha6), cos(q7)*cos(alpha6), -sin(alpha6), -sin(alpha6)*d7],
+						   [sin(q7)*sin(alpha6), cos(q7)*sin(alpha6),  cos(alpha6),  cos(alpha6)*d7],
+						   [                  0,                   0,            0,               1]])
+			T6_G = T6_7.subs(s)
+
+
+
 
             # Create individual transformation matrices
+
 			T0_1 = DHTransMatrix(alpha0, a0, d1, q1)
 			T0_1 = T0_1.subs(s)
 
@@ -123,9 +167,9 @@ def handle_calculate_IK(req):
 							  [0, 0, 1]])
 					return Mat_z
 			#Construct rotation matrix from roll pitch and yaw and extract the WC position
-			wc_x = cos(yaw)*sin(pitch)*cos(roll)+sin(yaw)*sin(roll)
-			wc_y = sin(yaw)*sin(pitch)*cos(roll)-cos(yaw)*sin(roll)
-			wc_z = cos(pitch)*cos(roll)
+			wc_x = d7*(cos(yaw)*sin(pitch)*cos(roll)+sin(yaw)*sin(roll))
+			wc_y = d7*(sin(yaw)*sin(pitch)*cos(roll)-cos(yaw)*sin(roll))
+			wc_z = d7*(cos(pitch)*cos(roll))
 
 
 
